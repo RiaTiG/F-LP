90076fc272e1ddd8f9bfe72f70e4f079bf67d170
RiaTiG <raker_2003@mail.ru>
2024-05-20 04:23:52 +0300

    Lab 5.3 Построено 6 функций, решающих задачи предыдущего задания с помощью рекурсии вверх и хвостовой рекурсии

--- src/Kotlin_Lab5_Func.kt
+++ src/Kotlin_Lab5_Func.kt
@@ -1,6 +1,3 @@
-import java.lang.System.`in`
-import java.util.*
-
 class Kotlin_Lab5_Func {
 
     fun minNum(num: Int): Int{
@@ -39,23 +36,63 @@ class Kotlin_Lab5_Func {
         return count
     }
 
-    fun minNumRec(num: Int): Int =
-        if (num < 10) num else if (num % 10 < minNumRec(num / 10))
-             num % 10 else minNumRec(num / 10)
+    //Найти минимальную цифру числа. Рекурсия вверх
+    fun minNumRecUP(num: Int): Int =
+        if (num < 10) num else if (num % 10 < minNumRecUP(num / 10))
+             num % 10 else minNumRecUP(num / 10)
 
-
-    fun kolNum3Rec(num: Int): Int =
+    //Найти количество цифр числа <3. Рекурсия вверх
+    fun kolNum3RecUP(num: Int): Int =
         if (num == 0 ) 0 else if (num < 10 && num < 3) 1 else
-            if (num % 10 < 3 ) 1 + kolNum3Rec(num / 10)
-            else kolNum3Rec(num / 10)
+            if (num % 10 < 3 ) 1 + kolNum3RecUP(num / 10)
+            else kolNum3RecUP(num)
 
-    fun delsNumRec(num: Int): Int =
-        if ()
+    //Найти количество делителей числа. Рекурсия вверх
+    fun delsNumRecUP(num: Int, divisor: Int = 1): Int {
+        if (divisor > num / 2) {
+            return if (num % divisor == 0) 2 else 1
+        }
+        if (num % divisor == 0) {
+            return 1 + delsNumRecUP(num, divisor + 1)
+        } else {
+            return delsNumRecUP(num, divisor + 1)
+        }
+    }
+
+    //Найти минимальную цифру числа. Хвостовая рекурсия
+    tailrec fun minNumRecTail(n: Int, minDigit: Int = 9): Int {
+        if (n == 0) {
+            return minDigit
+        }
+        val currentDigit = n % 10
+        return minNumRecTail(n / 10, if (currentDigit < minDigit) currentDigit else minDigit)
+    }
+
+    //Найти количество цифр числа <3. Хвостовая рекурсия
+    tailrec fun kolNum3RecTail(num: Int, count: Int = 0): Int {
+        if (num == 0) {
+            return count
+        }
+        val currentDigit = num % 10
+        return kolNum3RecTail(num / 10, if (currentDigit < 3) count + 1 else count)
+    }
+
+    //Найти количество делителей числа. Хвостовая рекурсия
+    tailrec fun delsNumRecTail(num: Int, divisor: Int = 1, count: Int = 0): Int {
+        if (num < divisor) {
+            return count
+        }
+        return if (num % divisor == 0) {
+            delsNumRecTail(num, divisor + 1, count + 1)
+        } else {
+            delsNumRecTail(num, divisor + 1, count)
+        }
+    }
 
     fun main() {
-        val scanner = Scanner(`in`)
-        val x: Int = scanner.nextInt()
-        println(kolNum3Rec(x))
+        //val scanner = Scanner(`in`)
+        //val x: Int = scanner.nextInt()
+        println()
     }
 }
 
