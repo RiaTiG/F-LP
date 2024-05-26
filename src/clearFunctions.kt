e8509ec8abbaa7946b97c3963399e68367f42aca
RiaTiG <raker_2003@mail.ru>
2024-05-26 22:28:00 +0300

    Lab 6.3 Добавлены функции: количество элементов, расположенных после последнего максимального. переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами. разместить элементы, расположенные до минимального, в конце массива. циклический сдвиг элементов массива вправо на одну позицию. циклический сдвиг элементов массива влево на одну позицию. максимальный нечетный элемент.

--- src/clearFunctions.kt
+++ src/clearFunctions.kt
@@ -12,7 +12,7 @@ class сlearFunctions(){
         }
     }
 
-    // Число элементов списка, которые могут быть квадратами другого числа
+    // Число элементов списка, которые могут быть квадратами другого числа.
     fun squareList(list: List<Int>): Int {
         return arrayOP(list, { count, el ->
             if (findInList(list, { it == el * el })) count + 1 else count
@@ -31,8 +31,53 @@ class сlearFunctions(){
                 )
             }
     }
+
+    //количество элементов, расположенных после последнего максимального.
+    fun elementsAfterMax(list: List<Int>): Int {
+        val lastIndex = list.indexOfLast { it == list.maxOrNull() }
+        return list.drop(lastIndex + 1).count()
+    }
+
+    //переставить в обратном порядке элементы массива, расположенные между его минимальным и максимальным элементами.
+    fun reverseMinMax(list: List<Int>): List<Int> {
+        val minIndex = list.indexOf(list.minOrNull())
+        val maxIndex = list.indexOf(list.maxOrNull())
+        val (firstIndex, lastIndex) = if (minIndex < maxIndex) minIndex to maxIndex else maxIndex to minIndex
+        val beforeMinMax = list.subList(0, firstIndex + 1)
+        val minMax = list.subList(firstIndex + 1, lastIndex).reversed()
+        val afterMinMax = list.subList(lastIndex, list.size)
+        return beforeMinMax + minMax + afterMinMax
+    }
+
+    //разместить элементы, расположенные до минимального, в конце массива.
+    fun moveMin(list: List<Int>): List<Int> {
+        val minIndex = list.indexOf(list.minOrNull())
+        val beforeMin = list.subList(0, minIndex)
+        val afterMin = list.subList(minIndex, list.size)
+        return afterMin + beforeMin
+    }
+
+    //циклический сдвиг элементов массива вправо на одну позицию.
+    fun goRight(list: List<Int>): List<Int> {
+        val lastElement = list.last()
+        val allNoLast = list.dropLast(1)
+        return listOf(lastElement) + allNoLast
+    }
+
+    //циклический сдвиг элементов массива влево на одну позицию.
+    fun goLeft(list: List<Int>): List<Int> {
+        val firstElement = list.first()
+        val allNoFirst = list.drop(1)
+        return allNoFirst + listOf(firstElement)
+    }
+
+    //максимальный нечетный элемент.
+    fun maxOdd(list: List<Int>): Int {
+        return list.filter { it % 2 != 0 }.maxOrNull()!!
+    }
+
 }
 fun main(){
     val c = сlearFunctions()
-    println(c.thirdList(listOf(3,15,75), listOf(3,25,35),listOf(9,11,21)))
+    println(c.maxOdd(listOf(3,15,75,15,17)))
 }
